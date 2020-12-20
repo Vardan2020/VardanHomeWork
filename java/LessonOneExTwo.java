4b88e2d830798df6ba40578d0b3429a8eb92be94
Galstyan <sovorakanlogin@mail.ru>
2020-12-20 22:12:11 +0400

    LesonOneExTwo

--- java/LessonOneExTwo.java
+++ java/LessonOneExTwo.java
@@ -1,8 +1,20 @@
 public class LessonOneExTwo {
     public static void main(String[] args) {
 
+     int x=1;
 
-
-    }
+     if ( x>0 ) {
+         int y = 2 * x - 1;
+         System.out.println(y);
+     }
+     if (x==0) {
+         int y = 0;
+         System.out.println(y);
+     }
+     if(x<0) {
+         int y = 3 * 2 * x - 5;
+         System.out.println(y);
+     }
+     }
 
 }
