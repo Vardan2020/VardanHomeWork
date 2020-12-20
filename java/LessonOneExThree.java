db2420069998730d92cbe96503f6ca51d72858f2
Galstyan <sovorakanlogin@mail.ru>
2020-12-21 01:33:05 +0400

    LesonOneExThree

--- java/LessonOneExThree.java
+++ java/LessonOneExThree.java
@@ -1,3 +1,23 @@
 public class LessonOneExThree {
 
+    public static void main(String[] args) {
+        int x=-3;
+        if (x>0) { int y=2*x-10;
+        System.out.println(y);
+
+        }
+        if (x==0) { int y=0;
+            System.out.println(y);
+
+        }
+        if (x<0) { int y=2*x-3;
+            System.out.println(y);
+
+        }
+
+
+
+
+    }
+
 }
