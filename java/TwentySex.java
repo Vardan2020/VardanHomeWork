50db6e22b440bb8f1e75e03b7eec8a585a73e097
Galstyan <sovorakanlogin@mail.ru>
2021-01-21 18:33:19 +0400

    TwentySex

---
+++ java/TwentySex.java
@@ -0,0 +1,15 @@
+public class TwentySex {
+    public static void main(String[] args) {
+            int a = 34;
+            int b = 27;
+            int c = 31;
+
+            if ((a % 2 == 0) || (b % 2 == 0) || (c % 2 == 0)) {
+                System.out.println("mek");
+            } else {
+                System.out.println("erku");
+            }
+
+
+        }
+    }
