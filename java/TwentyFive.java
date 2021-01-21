2b5b0237cc12c8fe972777187d5592956fd5fcf6
Galstyan <sovorakanlogin@mail.ru>
2021-01-21 18:24:45 +0400

    TwentyFive

---
+++ java/TwentyFive.java
@@ -0,0 +1,15 @@
+public class TwentyFive {
+    public static void main(String[] args) {
+        int a = 6;
+        int b = 9;
+        int c = 4;
+
+        if ((a+b)>c || (a+c)>b || (b+c)>a) {
+            System.out.println("y=1");
+        }else {
+            System.out.println("y=2");
+        }
+
+
+    }
+}
