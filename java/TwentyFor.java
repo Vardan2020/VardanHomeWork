81e405537a4452efb9373abdb1001b450addaa92
Galstyan <sovorakanlogin@mail.ru>
2021-01-21 18:20:33 +0400

    TwentyFor

---
+++ java/TwentyFor.java
@@ -0,0 +1,16 @@
+public class TwentyFor {
+    public static void main(String[] args) {
+        int a=204;
+        int b=2;
+        int c=3;
+
+
+        if ((a==2 & b==2) || (a==2 & c==2) || (b==2 & c==2)){
+            System.out.println(true);
+        }
+        else {
+            System.out.println(false);
+        }
+
+    }
+}
